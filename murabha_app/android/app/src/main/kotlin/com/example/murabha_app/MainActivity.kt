94e216d9f6f14573c0815d6c1e1977431c54397e
Omar Mansour <102729790+OmarMansour8@users.noreply.github.com>
2025-07-10 18:59:35 +0300

    - created the basic structure of the project - created the app router - created the colors manager - created the main function that run the app - added the dependencies in the pubspec.yml

---
+++ murabha_app/android/app/src/main/kotlin/com/example/murabha_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.murabha_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ murabha_app/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:murabha_app/core/routing/app_router.dart';
+import 'package:murabha_app/murabha_app.dart';
+
+void main() {
+  runApp(MurabhaApp(appRouter: AppRouter()));
+}
