d278bb5c5aea5b50368bb5a149e96abbad524745
MohamedAmr07 <m.amr@masaref-bsc.com>
2025-07-20 20:19:16 +0300

    Fix the Issue

--- murabha_app/lib/core/routing/app_router.dart
+++ murabha_app/lib/core/routing/app_router.dart
@@ -5,9 +5,11 @@ import 'package:murabha_app/core/routing/routes.dart';
 import 'package:murabha_app/features/browse_car/ui/browse_car.dart';
 import 'package:murabha_app/features/car_application/ui/application_form.dart';
 import 'package:murabha_app/features/home/ui/home_Screen.dart';
-import 'package:murabha_app/features/login/logic/cubit/login_cubit_cubit.dart';
-import 'package:murabha_app/features/login/ui/login_screen.dart';
-import 'package:murabha_app/features/login/ui/otp_screen.dart';
+import 'package:murabha_app/features/login/logic/login/cubit/login_cubit_cubit.dart';
+import 'package:murabha_app/features/login/logic/otp/cubit/otp_cubit.dart';
+import 'package:murabha_app/features/login/ui/login/login_screen.dart';
+import 'package:murabha_app/features/login/ui/otp/otp_screen.dart';
+import 'package:murabha_app/features/notification_list/ui/notify_list_screen.dart';
 import 'package:murabha_app/features/onboarding/ui/onboarding_screen.dart';
 import 'package:murabha_app/features/carList/ui/list_of_car_screen.dart';
 
@@ -29,11 +31,18 @@ class AppRouter {
       case Routes.carBrowseScreen:
         return MaterialPageRoute(builder: (_) => BrowseCar());
       case Routes.otpScreen:
-        return MaterialPageRoute(builder: (_) => OtpScreen());
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => getIt<OtpCubit>(),
+            child: OtpScreen(),
+          ),
+        );
       case Routes.applicationForm:
         return MaterialPageRoute(builder: (_) => ApplicationForm());
       case Routes.listCarScreen:
         return MaterialPageRoute(builder: (_) => ListOfCarScreen());
+      case Routes.notificationListScreen:
+        return MaterialPageRoute(builder: (_) => NotificationListScreen());
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
